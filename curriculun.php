fda0f5b95c0db9911e66d6b016a5319aca651a06
Dexter Rodriguez <Dexterrock48@gmail.com>
2021-04-28 15:34:24 -0500

    tarea 1.4

---
+++ curriculun.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="./assets/styles/style-barravertical.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400;700&display=swap" rel="stylesheet">
+    <scrip src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js">
+        </script>
+
+        <title>TallerWEB</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixel-top">
+        <div class="container">
+
+            <a class="navbar-brand" href="#"><img src="./assets/static//Dexter_Logo3.png" class="logo-brand" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <ion-icon name="menu-outline"></ion-icon>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="index2.php" id="Exit">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section id="portafolio">
+        <div class="container-fluid">
+            <div class="content-center">
+                <h1 name="curriculum">Curriculum</h1>
+                <img class="foto" src="./assets/static/Foto_redondo.png">
+
+                <br>
+
+                <h3>Datos personales</h3>
+                <Strong>Nombre:</Strong>
+                <p>Rodríguez Córdova Dexter Israel</p>
+                <strong>Fecha nacimiento: </strong>
+                <p> 25/03/1995 </p>
+                <strong>Correo: </strong>
+                <p>Dexterrock48@gmail.com</p>
+                <strong>Numero: </strong>
+                <p>9983179980</p>
+                <br>
+
+                <h3>Estudios Academicos</h3>
+
+                <strong>Preparatoria</strong>
+                <p> Colegio de Estudios Cientificos y Tecnologicos del estado de QRoo.</p>
+                <p> Tecnico en Soporte y Mantenimiento de Equipos de Computo </p>
+                <br>
+                <strong>Universidad</strong>
+                <p>Tecnologico de Cancún</p>
+                <p> Ing. Sistemas computacionales. </p>
+
+                <br>
+                <h3>Conocimientos de desarrollador de aplicaciones</h3>
+                <p> java / C# / Php / AndroidStudio / Html / Css / Sql </p>
+
+            </div>
+
+            <!-- Optional JavaScript; choose one of the two! -->
+            <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
--- index2.php
+++ index2.php
@@ -7,8 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
- <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
-  <link href="./assets/styles/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> 
+  <link href="./assets/styles/style-barravertical.css" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400;700&display=swap" rel="stylesheet">
   <scrip src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js">
@@ -20,6 +20,11 @@
 <body>
   <nav class="navbar navbar-expand-lg fixel-top">
     <div class="container">
+    <div class="btn-menu">
+       <label for="btn-menu" class="icon-menu">☰</label> 
+      
+      </div>
+
       <a class="navbar-brand" href="#"><img src="./assets/static//Dexter_Logo3.png" class="logo-brand" alt="logo"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <ion-icon name="menu-outline"></ion-icon>
@@ -27,19 +32,32 @@
 
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
-    <!--      <li class="nav-item">
-            <a class="nav-link" href="index2.php" id="home"> Home </a>
-          </li> -->
+   
           <li class="nav-item">
             <a class="nav-link" href="portafolio.php" id="portafolio">Portafolio</a>
-     <!--     </li>
+          </li>
           <li class="nav-item">
             <a class="nav-link" href="logout.php" id="Exit">Salir</a>
-          </li>   -->
+          </li> 
         </ul>
       </div>
     </div>
   </nav>
+
+  <input type="checkbox" id="btn-menu">
+<div class="container-menu">
+	<div class="cont-menu">
+		<nav>
+      <a href="index2.php">Inicio</a>
+			<a href="./curriculun.php">Actividad 1-1</a>
+			<a href="./index.php">Actividad 1-2</a>
+			<a href="./portafolio.php">Actividad 1-3</a>
+			<a href="#">Actividad 1-4</a>
+			<a href="#">Actividad 1-5</a> 
+		</nav>
+		<label for="btn-menu">✖️</label>
+	</div>
+</div>
 
   <section id="hero">
     <div class="container">
@@ -55,38 +73,17 @@
   </section>
   <section id="portafolio">
     <div class="container-fluid">
-<!--
+
       <div class="content-center">
-        <h1>Curriculum</h1>
+
+        <h1 name="curriculum">Alumno</h1>
         <img class="foto" src="./assets/static/Foto_redondo.png">
 
         <br>
-
-        <h3>Datos personales</h3>
-        <Strong>Nombre:</Strong>
         <p>Rodríguez Córdova Dexter Israel</p>
-        <strong>Fecha nacimiento: </strong>
-        <p> 25/03/1995 </p>
-        <strong>Correo: </strong>
-        <p>Dexterrock48@gmail.com</p>
-        <strong>Numero: </strong>
-        <p>9983179980</p>
-        <br>
-
-        <h3>Estudios Academicos</h3>
-
-        <strong>Preparatoria</strong>
-        <p> Colegio de Estudios Cientificos y Tecnologicos del estado de QRoo.</p>
-        <p> Tecnico en Soporte y Mantenimiento de Equipos de Computo </p>
-        <br>
-        <strong>Universidad</strong>
         <p>Tecnologico de Cancún</p>
         <p> Ing. Sistemas computacionales. </p>
 
-        <br>
-        <h3>Conocimientos de desarrollador de aplicaciones</h3>
-        <p> java / C# / Php / AndroidStudio / Html / Css / Sql </p>
--->
       </div>
       <!--  
       <div class="row1">
