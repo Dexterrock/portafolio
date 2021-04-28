30e6cc2f9b2b830492e2241927ed89614f403f5a
Dexter Rodriguez <Dexterrock48@gmail.com>
2021-04-28 11:46:12 -0500

    tarea 1.3 / se agregaron correciones a el menu vertical

--- 2dabarra.php
+++ 2dabarra.php
@@ -8,7 +8,7 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link href="./assets/styles/style.css" rel="stylesheet">
+  <link href="./assets/styles/style-barravertical.css" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200;400;700&display=swap" rel="stylesheet">
   <scrip src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js">
